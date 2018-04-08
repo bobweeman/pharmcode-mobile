e04bdbb9dafc3a6bbdb86d895189675906a074c7
Akrong Benjamin <akrongeconnect@gmail.com>
2018-04-08 11:13:08 +0000

    Add sign-in

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,7 +1,10 @@
 import { TabsPage } from './../tabs/tabs';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AppError } from '../common/app-error';
+import { Unauthorised } from '../common/unauthorised';
+
 
 /**
  * Generated class for the SignInPage page.
@@ -20,10 +23,26 @@ export class SignInPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignInPage');
+  //required credentials for receiving JWT from api-endpoint
+
+  credentials = {
+    email: '',
+    password: '',
+    grant_type: 'password',
+    client_secret: 'yEwte07tHoMKTO9Kimb7j2rf3g0EKK0mV0DI7rnx',
+    client_id: '2'
   }
 
+  //create a new relative form of the login
+  signinForm=new FormGroup({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    password:new FormControl(null, [Validators.minLength(8), Validators.required]),
+  });
 
 
+  authenticate(){
+    this.credentials.email=this.signinForm.controls['email'].value;
+    this.credentials.password=this.signinForm.controls['password'].value;
+   
+  }
 }
