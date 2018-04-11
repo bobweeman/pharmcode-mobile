07ab555d626445f8a30d3903040324ae24ac30f0
Akrong Benjamin <akrongeconnect@gmail.com>
2018-04-11 19:20:50 +0000

    Modified login page

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,9 +1,10 @@
 import { TabsPage } from './../tabs/tabs';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { AppError } from '../common/app-error';
 import { Unauthorised } from '../common/unauthorised';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 
 /**
@@ -20,11 +21,20 @@ import { Unauthorised } from '../common/unauthorised';
 })
 export class SignInPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  response: any;
+  acc_lvl: boolean = false;
+  signing: boolean = false;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private auth: AuthServiceProvider, private toastCtrl: ToastController, private fb: FormBuilder, private storage: Storage) {
   }
 
-  //required credentials for receiving JWT from api-endpoint
+  // sign-up page
+  signUp(){
+    this.navCtrl.push("SignUpPage");
+  }
 
+
+  //required credentials for receiving JWT from api-endpoint
   credentials = {
     email: '',
     password: '',
@@ -43,6 +53,110 @@ export class SignInPage {
   authenticate(){
     this.credentials.email=this.signinForm.controls['email'].value;
     this.credentials.password=this.signinForm.controls['password'].value;
-   
   }
+
+  signin() {
+    this.signing=true;
+    this.authenticate();
+    this.auth.postStore("authenticate", this.credentials, localStorage.getItem('token')).then((result) => {
+      this.response = result['token'];
+      if (this.response != null || this.response != undefined) {
+        localStorage.setItem('token', this.response)
+        localStorage.setItem('firstTimer','0');
+        
+        setTimeout(() => {
+          this.setUserId();
+          this.setUserAccessLevel()
+          this.signing = false;
+          this.navCtrl.setRoot("TabsPage");
+        }, 3000)
+
+
+      } else {
+        this.presentToast('Invalid username or password combination');
+        this.signing = false;
+
+        // this.loading.dismissAll();
+      }
+
+    }).catch((error) => {
+      if (error.status == 401) {
+        this.presentToast('Invalid username or password combination');
+        this.signing = false;
+
+      } else if (error.status === 0) {
+        this.presentToast('Please check your internet connection');
+        this.signing = false;
+      } else {
+        this.presentToast("Service not available. Please try again later");
+        this.signing = false;
+      }
+
+    });
+  }
+
+  setUserId() {//put logged in user id in localstorage
+    this.auth.getAll('authenticate', localStorage.getItem('token')).then((response) => {
+      localStorage.setItem('logUserId', response['user']['id']);  
+    }).catch((error) => { 
+      console.log(error);
+    });
+  }
+
+  setUserAccessLevel() {//put logged in user access_level in localstorage
+    this.auth.getSingle('access_level',localStorage.getItem('logUserId'),localStorage.getItem('token')).then((response) => {
+      localStorage.setItem('logUserAccessLevel', response['data']['uac_id']);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+
+  checkUserPriviledge() {
+    let a = JSON.parse(localStorage.getItem('logUserAccessLevel'));
+    switch (a) {
+
+      case 1:
+        // User is a Doctor 
+        this.acc_lvl = true;
+        // keep track of who has the ability to access doctor section
+        localStorage.setItem('canAccessDoctor',JSON.stringify(this.acc_lvl));
+        break;
+
+      case 2:
+        // User is a Pharmacist
+        this.acc_lvl = true;
+        // keep track of who has the ability to access pharmacist section
+        localStorage.setItem('canAccessPharmacist', JSON.stringify(this.acc_lvl));
+        break;
+
+      case 3:
+        // User is Admin 
+        this.acc_lvl = true;
+        // keep track of who has is the Admin and can access administrative tools
+        localStorage.setItem('canAccessAdmin', JSON.stringify(this.acc_lvl));
+        break;
+
+      case 4:
+        // User is a Normal User
+        this.acc_lvl = false;
+        break;
+
+      default:
+        this.acc_lvl = false;
+        break;
+    }
+
+  }
+
+
+  presentToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 5000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
 }
